8d057966632a59844ce972b93993f4a5ea84631e
Earl Escano <devdotearl@gmail.com>
2024-11-18 20:59:07 +0000

    Added first python script

---
+++ first.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# Author:
+# Version:
+# Description: our first python script
+
+name = input("Enter your Name: ")
+
+print("my name is ", name)
+
+import random
+print("my lucky number is", random.randint(1, 50))
+
+import math
+print("Cosine of 0.5 is", math.cos(0.5))
