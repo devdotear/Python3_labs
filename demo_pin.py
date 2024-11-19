adfce8feb164b67a4798828cc5060ed1feac7982
Earl Escano <devdotearl@gmail.com>
2024-11-19 19:29:32 +0000

    exercise for lab day 2

---
+++ demo_pin.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+# Author: Earl E
+# Version: 1.0
+# Description:
+
+master_pin = "0123"
+pin = None
+attempts = 0
+
+
+while pin != master_pin and attempts < 3:
+    pin = input("Enter PIN: ")
+    if pin == master_pin:
+        print("Valid PIN")
+        break
+    else:
+        print("Invalid PIN")
+        attempts += 1
+
+
+print("Done.")
