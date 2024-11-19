69c10ad993d9511401385f7da6a7b2f0a064058e
Earl Escano <devdotearl@gmail.com>
2024-11-19 23:33:10 +0000

    exercises and demo on lab day 2

---
+++ demo_str_format.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+# Author: EarlE
+# Version: 1.0
+# Description: This script will demo different ways of formatting strings
+"""
+    Docstring:
+"""
+
+
--- labs/sep.py
+++ labs/sep.py
@@ -1,4 +1,9 @@
 #! /usr/bin/python
 
 Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
+BelgiumList = Belgium.split(",")
 
+print("-" * len(Belgium))
+print(":".join(BelgiumList))
+print(int(BelgiumList[1]) + int(BelgiumList[3]))
+print("-" * len(Belgium))
