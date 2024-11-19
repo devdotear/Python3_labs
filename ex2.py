4d4310439100aaf49dd00fb669d2aeea363f5d64
Earl Escano <devdotearl@gmail.com>
2024-11-19 00:55:03 +0000

    Exercise 2 scripts

---
+++ ex2.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+# Author:
+# Version:
+# Description:
+
+first_name = "Earl"
+last_name = "Escano"
+
+print(first_name, last_name)
+
+name = [first_name, last_name]
+
+print(name)
+
+name = {"first": first_name, "last": last_name}
+
+print(name)
